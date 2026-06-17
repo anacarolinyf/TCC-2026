7db20d5548081a9f82e59577d4635facbfea4904
helolrodrigues <161508995+helolrodrigues@users.noreply.github.com>
2026-06-16 23:01:48 -0300

    Add files via upload
    mudança das paginas

---
+++ index.php
@@ -0,0 +1,87 @@
+<?php include 'includes/header.php';?>
+
+  <main>
+<section class="hero">
+
+    <div class="hero-texto">
+        <h1>
+            Informação, orientação e acolhimento para toda a família.
+        </h1>
+
+        <p>
+            Encontre conteúdos confiáveis sobre o Transtorno do
+            Espectro Autista (TEA), direitos, profissionais de apoio
+            e orientações para cada etapa da jornada.
+        </p>
+
+        <div class="botoes">
+            <a href="paginas/guia.html" class="btn-principal">
+                Começar agora
+            </a>
+
+            <a href="paginas/leis.html" class="btn-secundario">
+                Conheça seus direitos
+            </a>
+        </div>
+    </div>
+
+    <div class="hero-imagem">
+    <img src="img/banner.png" alt="Família">
+</div>
+
+</section>
+
+<section class="cards">
+
+    <div class="card">
+        <i class="fa-solid fa-puzzle-piece"></i>
+        <h3>Entenda o TEA</h3>
+        <p>Informações básicas sobre autismo.</p>
+    </div>
+
+    <div class="card">
+        <i class="fa-solid fa-users"></i>
+        <h3>Guia para Famílias</h3>
+        <p>Orientações para o dia a dia.</p>
+    </div>
+
+    <div class="card">
+        <i class="fa-solid fa-scale-balanced"></i>
+        <h3>Direitos</h3>
+        <p>Leis e benefícios garantidos.</p>
+    </div>
+
+    <div class="card">
+        <i class="fa-solid fa-book"></i>
+        <h3>Biblioteca</h3>
+        <p>Materiais e conteúdos úteis.</p>
+    </div>
+
+</section>
+
+<section class="apoio">
+
+    <div>
+        <h2>Recebeu um diagnóstico recente?</h2>
+
+        <p>
+            Conheça um caminho inicial para compreender o
+            diagnóstico, buscar apoio profissional e acessar
+            informações confiáveis.
+        </p>
+
+        <a href="paginas/guia.html" class="btn-principal">
+            Ver Guia Completo
+        </a>
+    </div>
+
+</section>
+
+</main>
+
+<?php include 'includes/footer.php'; ?>
+  </main>
+
+</body>
+
+</html>
